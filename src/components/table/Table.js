357f397a8d39d5625059d914d9b5c9b6e2cd251c
vprokudin <vprokudin@tsconsulting.com>
2020-08-26 22:01:30 +0300

    resize table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,24 @@
-import { ExcelComponent } from "../../core/ExcelComponent";
-import { createTable } from "./table.template";
+import { ExcelComponent } from '@/core/ExcelComponent';
+import { createTable } from './table.template';
+import resizeHandler from '@/components/table/resizeHandler';
+import isResizer from '@/utils/isResizer';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table';
 
+    constructor($root) {
+        super($root, {
+            listeners: ['mousedown'],
+        });
+    }
+
     toHTML() {
         return createTable(20);
+    }
+
+    onMousedown(e) {
+        if (isResizer(e)) {
+            resizeHandler(this.$root, e);
+        }
     }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,21 +1,31 @@
 const CODES = {
     A: 65,
-    Z: 90
-}
+    Z: 90,
+};
 
-function toCell(content) {
+function toCell(content, i) {
+    console.log('toCell', content);
     return `
-    <div class="cell" contenteditable>${content}</div>
+    <div class="cell" contenteditable data-column="${toChar(null, i)}">${content}</div>
     `;
 }
 
 function toColumn(col) {
-    return `<div class="column">${col}</div>`;
+    return `
+        <div class="column" data-type="resizable">
+            ${col}
+            <div class="col-resize" data-resize="col"></div>
+        </div>`;
 }
 
 function createRow(content, i) {
-    return `<div class="row">
-                <div class="row-info">${i}</div>
+    const resize = i ? '<div class="row-resize" data-resize="row"></div>' : '';
+
+    return `<div class="row" data-type="resizable">
+                <div class="row-info">
+                    ${i}
+                    ${resize}
+                </div>
                 <div class="row-data">${content}</div>
             </div>`;
 }
@@ -27,11 +37,7 @@ function toChar(_, i) {
 export function createTable(rowsCount = 15) {
     const colsCount = CODES.Z - CODES.A + 1;
     const rows = [];
-    const cols = new Array(colsCount)
-        .fill('')
-        .map(toChar)
-        .map(toColumn)
-        .join('');
+    const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('');
     const cells = new Array(colsCount).fill('').map(toCell).join('');
 
     rows.push(createRow(cols, ''));
